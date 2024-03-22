9dab3388180fedcf68ad8e28a165348686e29c6e
etvorellana <evalero@uesc.br>
2024-03-22 15:37:50 -0300

    modif main_02.c

--- ExemplosAula/main_02.c
+++ ExemplosAula/main_02.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(){
     printf("Qual o seu nome!\n");
     char nome[100];
     fgets(nome, 100, stdin);
+    nome[strlen(nome)-1] = '\0'; // Remove o \n do final da string (fgets inclui o \n no final da string)
     printf("Ola %s", nome);
     return 0;
 }
